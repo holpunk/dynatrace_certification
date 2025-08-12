c971d45b1b1b8cb4dc6c2bf765891796bec2dd27
holpunk <aqshey@gmail.com>
2025-08-12 16:48:10 +0200

    oneAgent in docker container added and services exposed to specific ports

--- main.py
+++ main.py
@@ -3,14 +3,16 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
+def print_list():
+    my_list = [1, 2, 3, 4, 5]
+    my_list.append(6)
+    my_list.remove(2)
+    my_list.sort()
+    my_list.reverse()
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
+    print(f'Hi, {my_list}')  # Press ⌘F8 to toggle the breakpoint.
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    print_list()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
